f0c0c970f00c14b8ab152d58325c0748b3c36801
JerryLeandro <jerryleandro41@gmail.com>
2021-08-20 13:32:11 -0300

    classe impl e pacote de test

---
+++ src/main/java/impl/EventIteratorImpl.java
@@ -0,0 +1,32 @@
+package impl;
+
+import net.intelie.challenges.Event;
+import net.intelie.challenges.EventIterator;
+
+public class EventIteratorImpl implements EventIterator{
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean moveNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Event current() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/impl/EventStoreImpl.java
@@ -0,0 +1,27 @@
+package impl;
+
+import net.intelie.challenges.Event;
+import net.intelie.challenges.EventIterator;
+import net.intelie.challenges.EventStore;
+
+public class EventStoreImpl implements EventStore{
+
+	@Override
+	public void insert(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeAll(String type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EventIterator query(String type, long startTime, long endTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
